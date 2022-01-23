67bb0e8b349b87abd8f3d74b8793090c871dffc6
Hugo Vallada <valladahugo@gmail.com>
2022-01-23 10:21:48 -0300

    Ajuste de log

--- api-consumer/src/main/kotlin/adapters/gateway/CreatePokemonGateway.kt
+++ api-consumer/src/main/kotlin/adapters/gateway/CreatePokemonGateway.kt
@@ -1,18 +1,20 @@
 package adapters.gateway
 
+import adapters.utils.Colors
 import adapters.config.Environment
 import core.models.PokemonRequest
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class CreatePokemonGateway {
 
     companion object {
         suspend fun createPokemon(ktorClient: HttpClient) {
-
-            println("${LocalDateTime.now()} - INFO - User: ${Environment.name} - Password: ${Environment.password}")
+            println("${Colors.ANSI_RED.ansiValue}${LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT))} - ${this::class.java.declaringClass.simpleName} INFO: User: ${Environment.name} - Password: ${Environment.password}")
 
             ktorClient.request<Any>("http://localhost:8080/pokemons") {
                 method = HttpMethod.Post
---
+++ api-consumer/src/main/kotlin/adapters/utils/Colors.kt
@@ -0,0 +1,5 @@
+package adapters.utils
+
+enum class Colors(val ansiValue: String) {
+    ANSI_RED("\u001B[31m")
+}
